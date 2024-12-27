47880f47f6182b3e7893bfc354674af3a0bdffbd
tulik <tolqinmirsaliyev@gmail.com>
2024-12-26 20:57:51 -0500

    erererere

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,11 @@
-/** @type {import('tailwindcss').Config} */
+//** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}', // Sahifalar
+    './components/**/*.{js,ts,jsx,tsx}', // Komponentlar
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
