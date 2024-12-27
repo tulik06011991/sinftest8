a44891a3e1885abe84898c0d9a4bf9559c43faf6
tulik <tolqinmirsaliyev@gmail.com>
2024-12-26 21:04:02 -0500

    eeef

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,10 +1,13 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        {/* Faviconni ulash */}
+        <link rel="icon" href="/quiz.png" />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
