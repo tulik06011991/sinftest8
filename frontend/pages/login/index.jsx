5c0b8cc5e5f68cbd02ed5e8900a9086cdcef85fc
tulik <tolqinmirsaliyev@gmail.com>
2024-12-30 12:23:37 -0500

    ghghg

--- frontend/pages/login/index.jsx
+++ frontend/pages/login/index.jsx
@@ -6,7 +6,7 @@ const index = () => {
     <div class="bg-gray-100 flex justify-center items-center h-screen">
        
     <div class="w-1/2 h-screen hidden lg:block">
-      <img src="https://images.pexels.com/photos/8613059/pexels-photo-8613059.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Placeholder Image" class="object-cover w-full h-full"/>
+      <img src="https://images.pexels.com/photos/8617842/pexels-photo-8617842.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Placeholder Image" class="object-cover w-full h-full"/>
     </div>
     {/* <!-- Right: Login Form --> */}
     <div class="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
